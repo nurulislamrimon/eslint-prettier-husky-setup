9fa5e5257fed7fdbaa063caed44a44cfd91c40ac
nurulislamrimon <nurulislamrimon@gmail.com>
2023-06-14 19:46:41 +0600

    eslint by husky checking

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import colors from 'colors'
 import app from './app/app'
 import config from './config/config'
 
+const a = 10
+
 app.listen(config.port, () => {
   console.log(colors.magenta(`Example app listening on port ${config.port}`))
 })
